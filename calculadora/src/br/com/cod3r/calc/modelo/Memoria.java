723dd6f01ff81235da05e08883fedff604a6663e
EDUARDO DE JESUS MOREIRA <79948050+EduardoMoreira023@users.noreply.github.com>
2024-07-12 17:59:56 -0300

    novos arquivos!!!

--- calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
@@ -1,8 +1,17 @@
 package br.com.cod3r.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 	
+	private enum TipoComando {
+			ZERAR, NUMERO, DIVISAO, MULTIPLICACAO, SUBTRACAO, SOMA, IGUAL, VIRGULA;
+	};
+	
 	private static final Memoria instancia = new Memoria();
+	
+	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	
 	private String textoAtual = "";
 	
@@ -13,11 +22,35 @@ public class Memoria {
 	public static Memoria getInstancia() {
 		return instancia;
 	}
+	
+	public void adicionarObservador(MemoriaObservador observador) {
+		observadores.add(observador);
+	}
 
 	public String getTextoAtual() {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
 	
-	
+	public void processarComando(String texto) {
+		
+		TipoComando tipoComando = detectarTipoComando(texto);
+		
+		if("AC".equals(texto)) {
+			textoAtual = "";
+		}else {
+			textoAtual += texto;			
+		}
+		
+		observadores.forEach(o -> o.valorAlterado(getTextoAtual() ));
+	}
+
+	private TipoComando detectarTipoComando(String texto) {
+		
+		if(textoAtual.isEmpty() && texto == "0") {
+			return null;
+		}
+		
+		return null;
+	}
 
 }
