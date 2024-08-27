c23658066ce89ad9dcec80b48704308f45749827
EDUARDO DE JESUS MOREIRA <79948050+EduardoMoreira023@users.noreply.github.com>
2024-08-27 18:47:35 -0300

    new commits

---
+++ mysql/inserirPrefeitos.sql
@@ -0,0 +1,20 @@
+select * from cidades;
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 3),
+    ('Raquel Lyra', 7),
+    ('Zenaldo Coutinho', null);
+
+select * from prefeitos;
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rafael Greca', null);
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Pinhiro', 3);
